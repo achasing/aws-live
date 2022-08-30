6b5f1706e35d2b63a6390acb83fe3ebeda351c4f
achasing <67547753+achasing@users.noreply.github.com>
2022-08-30 10:45:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c3pxboqvh2pf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "changyowlap-employee"
+customregion = "us-east-1"
